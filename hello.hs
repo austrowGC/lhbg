2a1131b112023fd7ed33cbe2fb8e6ce1161cd7ca
austrowgc <gregoryaustrow@gmail.com>
2022-09-07 09:18:06 -0400

    work up to newtypes

--- hello.hs
+++ hello.hs
@@ -1,11 +1,43 @@
-opentag name = "<" <> name <> ">"
+main :: IO ()
+main = putStrLn html
 
-closetag name = "</" <> name <> ">"
+newtype Html = Html String
 
-markup tag text = (opentag tag) <> (text <> clostag tag)
+newtype Elem = Elem String
 
-body content = markup "body" content
+getHtmlString :: Html -> String
+getHtmlString (Html str) = str
 
-html content = markup "html" content
+getElemString :: Elem -> String
+getElemString (Elem str) = str
 
-main = putStrLn html body "Hello, World!"
+el :: String -> String -> String
+el tag inner =
+  "<" <> tag <> ">" <> inner <> "</" <> tag <> ">"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+title_ :: String -> String
+title_ = el "title"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+html_ :: String -> String
+html_ = el "html"
+
+doc :: String -> String -> String
+doc title body = html_ (head_ (title_ title) <> body_ body)
+
+html :: String
+html =
+  doc
+    "austrowGC/lhbg"
+    ((h1_ "Hello, World!") <> p_ "Learning Haskell Blog Generator")
