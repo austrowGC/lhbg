88784f2b1266d0e40773308f190f26fda225bae9
austrowgc <gregoryaustrow@gmail.com>
2022-11-03 20:04:16 -0400

    replace declarations with internal import add export of ol, ul, code

--- Html.hs
+++ Html.hs
@@ -5,58 +5,11 @@ module Html
     , html_
     , h1_
     , p_
+    , ul_
+    , ol_
+    , code_
     , append_
     , render
     )
     where
-
-newtype Html = Html String -- the whole document
-
-newtype Structure = Structure String -- a tag and its contents
-
-type Title = String
-
-getHtmlString :: Html -> String
-getHtmlString (Html str) = str
-
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
-
-el :: String -> String -> String
-el tag inner = "<" <> tag <> ">" <> inner <> "</" <> tag <> ">"
-
-p_ :: String -> Structure
-p_ = Structure . el "p" . escape
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1" . escape
-
-html_ :: Title -> Structure -> Html
-html_ title content = Html
-    (el "html"
-        (el "head" (el "title" (escape title))
-            <> el "body" (getStructureString content)
-        )
-    )
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
-
-render :: Html -> String
-render html =
-    case html of
-        Html string -> string
-
-escape :: String -> String
-escape =
-    let
-        escapeChar c =
-            case c of
-                '<' -> "&lt;"
-                '>' -> "&rt;"
-                '&' -> "&amp;"
-                '"' -> "&quot;"
-                '\'' -> "&#39;"
-                _ -> [c]
-    in
-        concat . map escapeChar
+import Html.Internal
