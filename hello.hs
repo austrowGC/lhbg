636269ddb539ce35657e9494ac25b73e52f009fd
austrowgc <gregoryaustrow@gmail.com>
2022-11-03 20:09:04 -0400

    add more 'my*' declarations for fun

--- hello.hs
+++ hello.hs
@@ -5,11 +5,17 @@ main = putStrLn (render myhtml)
 
 myhtml :: Html
 myhtml = html_
-    "austrowgc/lhbg"
+    "<whoa>'austrowgc/lhbg'</whoa>"
     (append_
         (h1_ "Hello, World!")
-        (append_
-            (p_ "paragraph #1")
-            (p_ "paragraph #2")
-        )
+        (append_ myorderedlist mylist)
     )
+
+myorderedlist :: Structure
+myorderedlist = ol_ [p_ "gravy", p_ "potatoes"]
+
+mylist :: Structure
+mylist = ul_
+    [ code_ "myorderedlist :: Structure\nmyorderedlist = ol_ [p_ \"gravy\", p_ \"potatoes\"]\n"
+    , code_ "main :: IO ()\nmain = putStrLn (render myhtml)\n"
+    ]
