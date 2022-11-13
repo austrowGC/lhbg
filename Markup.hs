61a09c31c90ad518e67a7548f4eef1d2c764ae7d
Gregoy Austrow <gregoryaustrow@gmail.com>
2022-11-13 15:07:04 -0500

    change parser to use Maybe

--- Markup.hs
+++ Markup.hs
@@ -20,17 +20,24 @@ data Structure
     | CodeBlock [String]
     deriving Show
 
-parse = parseLines [] . lines
+parse = parseLines Nothing . lines
 
-parseLines currentParagraph txts =
-    let paragraph = Paragraph $ unlines $ reverse currentParagraph
-    in
-        case txts of
-            [] -> [paragraph]
-            currentLine : rest ->
-                if trim currentLine == ""
-                    then paragraph : parseLines [] rest
-                    else parseLines (currentLine : currentParagraph) rest
+parseLines context txts =
+    case txts of
+        [] -> maybeToList context
+        currentLine : rest ->
+            let
+                line = trim currentLine
+            in
+                if line == ""
+                    then
+                        maybe id (:) context (parseLines Nothing rest)
+                    else
+                        case context of
+                            Just (Paragraph paragraph) ->
+                                parseLines (Just $ Paragraph $ unwords [paragraph, line]) rest
+                            _ ->
+                                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
 
 trim :: String -> String
 trim = unwords . words
