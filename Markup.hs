822808b7d7775fd3f5cb89681a2017a8016a5f2c
Gregoy Austrow <gregoryaustrow@gmail.com>
2022-11-16 12:13:02 -0500

    add parsing, remove data examples

--- Markup.hs
+++ Markup.hs
@@ -25,6 +25,20 @@ parse = parseLines Nothing . lines
 parseLines context txts =
     case txts of
         [] -> maybeToList context
+
+        -- Heading 1
+        ( '*' : ' ' : line ) : rest ->
+            maybe id (:) context $ Heading 1 (trim line) : parseLines Nothing rest
+
+        -- Unordered List
+        ( '-' : ' ' : line ) : rest ->
+            case context of
+                Just (UnorderedList list) ->
+                    parseLines (Just $ UnorderedList $ list <> [trim line]) rest
+                _ ->
+                    maybe id (:) context $ parseLines (Just $ UnorderedList [trim line]) rest
+
+        -- Paragraph
         currentLine : rest ->
             let
                 line = trim currentLine
@@ -56,60 +70,3 @@ exactlyTwo list =
     case list of
         [x, y] -> Just (x, y)
         _ -> Nothing
-
-
--- test data --
-
-ex1 :: Document
-ex1 =
-    [ Paragraph "Hello, World!"
-    ]
-
-ex2 :: Document
-ex2 =
-    [ Heading 1 "Welcome"
-    , Paragraph "To this tutorial about Haskell"
-    ]
-
-ex3 :: Document
-ex3 =
-    [ Paragraph ("something something remember " <> "multiple lines or something")
-    , OrdereedList
-        [ "Item 1 of a list"
-        , "Item 2, same list"
-        ]
-    ]
-
-ex4 :: Document
-ex4 =
-    [ Heading 1 "Compiling programs with ghc"
-    , Paragraph (
-        concat
-            [ "Running ghc invokes the Glasgow Haskell Compiler (GHC),\n"
-            , "and can be used to compile Haskell modules and programs into native\n"
-            , "executables and libraries.\n"
-            ]
-    )
-    , Paragraph "Create a new Haskell source file named hello.hs, and write the following code in it:"
-    , CodeBlock
-        [ "main = putStrLn \"Hello, Haskell!\""
-        ]
-    , Paragraph "Now, we can compile the program by invoking ghc with the file name:"
-    , CodeBlock
-        [ "➜ ghc hello.hs"
-        , "[1 of 1] Compiling Main             ( hello.hs, hello.o )"
-        , "Linking hello ..."
-        ]
-    , Paragraph "GHC created the following files:"
-    , UnorderedList
-        [ "hello.hi - Haskell interface file"
-        , "hello.o - Object file, the output of the compiler before linking"
-        , "hello (or hello.exe on Microsoft Windows) - A native runnable executable."
-        ]
-    , Paragraph "GHC will produce an executable when the source file satisfies both conditions:"
-    , OrdereedList
-        [ "Defines the main function in the source file"
-        , "Defines the module name to be Main, or does not have a module declaration"
-        ]
-    , Paragraph "Otherwise, it will only produce the .o and .hi files."
-    ]
