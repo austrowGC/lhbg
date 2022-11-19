be78c1732167a105c938c19e119b10c273c4b78b
Gregoy Austrow <gregoryaustrow@gmail.com>
2022-11-19 15:12:58 -0500

    init markup to html conversion module

---
+++ Convert.hs
@@ -0,0 +1,27 @@
+module Convert
+where
+
+import qualified Html
+import qualified Markup
+
+convert :: Html.Title -> Markup.Document -> Html.Html
+convert title = Html.html_ title . foldMap convertStructure
+
+convertStructure :: Markup.Structure -> Html.Structure
+convertStructure structure =
+    case structure of
+        Markup.Heading n txt ->
+            Html.h_ n txt
+
+        Markup.Paragraph txt ->
+            Html.p_ txt
+
+        Markup.UnorderedList list ->
+            Html.ul_ $ map Html.p_ list
+
+        Markup.OrderedList list ->
+            Html.ol_ $ map Html.p_ list
+
+        Markup.CodeBlock list ->
+            Html.code_ $ unlines list
+
